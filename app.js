dcb25b8e32f139b4e3a29015ddf94a0593637559
madhuryabezavada <madhuryabezavada96@gmail.com>
2023-12-21 06:07:21 +0000

    cricket team correct solution

--- app.js
+++ app.js
@@ -1,149 +1,105 @@
 const express = require("express");
-
-const path = require("path");
-
 const { open } = require("sqlite");
 const sqlite3 = require("sqlite3");
+const path = require("path");
+
+const databasePath = path.join(__dirname, "cricketTeam.db");
 
 const app = express();
 
 app.use(express.json());
 
-const dbPath = path.join(__dirname, "moviesData.db");
+let database = null;
 
-let db = null;
-
-const intialize = async () => {
+const initializeDbAndServer = async () => {
   try {
-    db = await open({
-      filename: dbPath,
+    database = await open({
+      filename: databasePath,
       driver: sqlite3.Database,
     });
     app.listen(3000, () =>
       console.log("Server Running at http://localhost:3000/")
     );
-  } catch (e) {
-    console.log(`DB error ${e.message}`);
-
-    prosess.exit(1);
+  } catch (error) {
+    console.log(`DB Error: ${error.message}`);
+    process.exit(1);
   }
 };
 
-intialize();
+initializeDbAndServer();
 
-const convertMoviesObjectToResponseObject = (dbObject) => {
+const convertDbObjectToResponseObject = (dbObject) => {
   return {
-    movieId: dbObject.movie_id,
-    directorId: dbObject.director_id,
-    movieName: dbObject.movie_name,
-    leadActor: dbObject.lead_actor,
+    playerId: dbObject.player_id,
+    playerName: dbObject.player_name,
+    jerseyNumber: dbObject.jersey_number,
+    role: dbObject.role,
   };
 };
 
-const convertDirectorObjectToResponseObject = (dbObject) => {
-  return {
-    directorId: dbObject.director_id,
-    directorName: dbObject.director_name,
-  };
-};
-
-app.get("/movies/", async (request, response) => {
-  const getMoviesQuery = `SELECT * FROM movie;`;
-
-  const moviesArray = await db.all(getMoviesQuery);
-
-  response.send(
-    moviesArray.map((eachMovie) => ({ movieName: eachMovie.movie_name }))
-  );
-});
-
-app.get("/movies/:movieId/", async (request, response) => {
-  const { movieId } = request.params;
-  const getMovieQuery = `
-    SELECT 
-     *
+app.get("/players/", async (request, response) => {
+  const getPlayersQuery = `
+    SELECT
+      *
     FROM
-      movie
-    WHERE
-      movie_id = ${movieId};`;
-  const movie = await db.get(getMovieQuery);
-  response.send(convertMoviesObjectToResponseObject(movie));
-});
-
-app.post("/movies/", async (request, response) => {
-  const details = request.body;
-  const { directorId, movieName, leadActor } = details;
-  const api2 = `
-     INSERT INTO
-     movie (director_id,movie_name,lead_actor) 
-     VALUES 
-     (
-          ${directorId},
-         '${movieName}',
-         '${leadActor}');`;
-  const db3 = await db.run(api2);
-
-  response.send("Movie Successfully Added");
-});
-
-app.put("/movies/:movieId/", async (request, response) => {
-  const { movieId } = request.params;
-  const details = request.body;
-  const { directorId, movieName, leadActor } = details;
-  const updateMovieQuery = `
-    UPDATE
-    movie
-    SET
-    director_id = ${directorId},
-    movie_name = '${movieName}',
-    lead_actor = '${leadActor}'
-    WHERE
-    movie_id = ${movieId};`;
-
-  await db.run(updateMovieQuery);
-  response.send("Movie Details Updated");
-});
-
-app.delete("/movies/:movieId/", async (request, response) => {
-  const { movieId } = request.params;
-  const deleteMovieQuery = `
-    DELETE 
-    FROM
-    movie
-    WHERE
-    movie_id = ${movieId};`;
-  await db.run(deleteMovieQuery);
-  response.send("Movie Removed");
-});
-
-app.get("/directors/", async (request, response) => {
-  const getDirectorsQuery = `SELECT * FROM director;`;
-
-  const directorArray = await db.all(getDirectorsQuery);
-
+      cricket_team;`;
+  const playersArray = await database.all(getPlayersQuery);
   response.send(
-    directorArray.map((eachDirector) =>
-      convertDirectorObjectToResponseObject(eachDirector)
+    playersArray.map((eachPlayer) =>
+      convertDbObjectToResponseObject(eachPlayer)
     )
   );
 });
 
-app.get("/directors/:directorId/movies", async (request, response) => {
-  const { directorId } = request.params;
-
-  const getDirectorMovieQuery = `
+app.get("/players/:playerId/", async (request, response) => {
+  const { playerId } = request.params;
+  const getPlayerQuery = `
     SELECT 
-    movie_name
-    FROM
-    movie
-    WHERE
-    director_id = ${directorId};`;
-
-  const moviesArray = await db.all(getDirectorMovieQuery);
-
-  response.send(
-    moviesArray.map((eachMovie) => ({ movieName: eachMovie.movie_name }))
-  );
+      * 
+    FROM 
+      cricket_team 
+    WHERE 
+      player_id = ${playerId};`;
+  const player = await database.get(getPlayerQuery);
+  response.send(convertDbObjectToResponseObject(player));
 });
 
+app.post("/players/", async (request, response) => {
+  const { playerName, jerseyNumber, role } = request.body;
+  const postPlayerQuery = `
+  INSERT INTO
+    cricket_team (player_name, jersey_number, role)
+  VALUES
+    ('${playerName}', ${jerseyNumber}, '${role}');`;
+  const player = await database.run(postPlayerQuery);
+  response.send("Player Added to Team");
+});
+
+app.put("/players/:playerId/", async (request, response) => {
+  const { playerName, jerseyNumber, role } = request.body;
+  const { playerId } = request.params;
+  const updatePlayerQuery = `
+  UPDATE
+    cricket_team
+  SET
+    player_name = '${playerName}',
+    jersey_number = ${jerseyNumber},
+    role = '${role}'
+  WHERE
+    player_id = ${playerId};`;
+
+  await database.run(updatePlayerQuery);
+  response.send("Player Details Updated");
+});
+
+app.delete("/players/:playerId/", async (request, response) => {
+  const { playerId } = request.params;
+  const deletePlayerQuery = `
+  DELETE FROM
+    cricket_team
+  WHERE
+    player_id = ${playerId};`;
+  await database.run(deletePlayerQuery);
+  response.send("Player Removed");
+});
 module.exports = app;
